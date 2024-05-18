f61c1edf95364d0e2dac0973931e7503e23cf98b
ryan <stanek.r77@gmail.com>
2024-05-18 16:40:25 -0500

    corrected logic in navigationvm

--- LabAutomata.Wpf.Library/src/viewmodel/NavigationVm.cs
+++ LabAutomata.Wpf.Library/src/viewmodel/NavigationVm.cs
@@ -2,17 +2,28 @@
 using LabAutomata.Wpf.Library.common;
 using LabAutomata.Wpf.Library.data_structures;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System.Windows.Threading;
 
 namespace LabAutomata.Wpf.Library.viewmodel {
+    /// <summary>
+    /// Represents a view model for navigation within the application.
+    /// </summary>
     public class NavigationVm : Base {
+        /// <summary>
+        /// Gets the command ID for the work request view model.
+        /// </summary>
         public string CmdIdWorkRequest { get; } = nameof(WorkRequestVm);
 
-        public Command ChangeVm { get; }
+        /// <summary>
+        /// Gets the command for changing the current view model.
+        /// </summary>
+        public CommandAsync ChangeVm { get; }
 
         private Base? _currentVm;
 
+        /// <summary>
+        /// Gets or sets the current view model.
+        /// </summary>
         public Base? CurrentVm {
             get => _currentVm;
             set {
@@ -22,27 +33,29 @@ namespace LabAutomata.Wpf.Library.viewmodel {
             }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NavigationVm"/> class.
+        /// </summary>
+        /// <param name="sp">The service provider.</param>
+        /// <param name="vmc">The view model collection.</param>
         public NavigationVm (IServiceProvider sp, ViewModelCollection vmc) : base(sp) {
             _vmc = vmc;
             var da = sp.GetService<IAdapter<Dispatcher>>();
 
-            //ChangeVm = new CommandAsync(da!.Get(), context: async newVm => {
-            //    Logger?.LogInformation("ChangeVm command heard!");
-            //    Logger?.LogInformation("Command passed: " + newVm);
-            //    await Task.Delay(3000);
-            //    var t = _vmc[(newVm as string)!];
-            //    CurrentVm = t;
-            //});
+            if (da == null)
+                throw new NullReferenceException(DispatcherNull);
 
-            ChangeVm = new Command(newVm => {
-                Logger?.LogInformation("ChangeVm command heard!");
-                Logger?.LogInformation("Command passed: " + newVm);
-                //await Task.Delay(3000);
-                var t = _vmc[newVm as string];
-                CurrentVm = t;
+            // TODO: could refactor this into its own class
+            ChangeVm = new CommandAsync(da.Get(), async vmIdObj => {
+                if (vmIdObj == null || vmIdObj is not string)
+                    return;
+
+                await Task.Delay(3000);
+                CurrentVm = _vmc[(string)vmIdObj];
             });
         }
 
         private readonly ViewModelCollection _vmc;
+        private const string DispatcherNull = "Dispatcher adapter was null.";
     }
 }
