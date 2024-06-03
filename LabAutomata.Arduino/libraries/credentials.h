b5647cb6d77578efe2904f6c3d07196ad24fded7
ryan-io <47833404+ryanunitydeveloper@users.noreply.github.com>
2024-06-03 11:37:53 -0500

    added to gitignore.

--- LabAutomata.Arduino/libraries/credentials.h
+++
@@ -1,7 +0,0 @@
-#define BLYNK_TEMPLATE_ID "TMPL2PQbaiSFl"
-#define BLYNK_TEMPLATE_NAME "Quickstart Template"
-#define BLYNK_AUTH_TOKEN "qYzut95mqbaRfksv91v368ZHrZszkjZh"
-
-char ssid[] = "NETGEAR81";
-char pass[] = "1901grovelane";
-char auth[] = BLYNK_AUTH_TOKEN;
