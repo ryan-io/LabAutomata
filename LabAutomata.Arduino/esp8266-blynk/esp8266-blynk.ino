2d24ffd499b3506428de3d9e98addc1697f2d67d
ryan-io <47833404+ryanunitydeveloper@users.noreply.github.com>
2024-06-04 12:29:16 -0500

    modified core readme. added reame to the arduino project.

--- LabAutomata.Arduino/esp8266-blynk/esp8266-blynk.ino
+++ LabAutomata.Arduino/esp8266-blynk/esp8266-blynk.ino
@@ -17,6 +17,7 @@
 //      LabAutomata\LabAutomata.Arduino\libraries\credentials\
 
 #include "credentials.h"
+#include "DHT.h"
 #include <ESP8266_Lib.h>
 #include <BlynkSimpleShieldEsp8266.h>
 #include <SoftwareSerial.h>
@@ -34,42 +35,53 @@
 SoftwareSerial EspSerial(2, 3); // TX, RX
 ESP8266 wifi(&EspSerial);
 BlynkTimer m_timer;
+DHT m_dhtSys1;
 
-const int m_led_pin_high=6;
-const int m_led_pin_low=5;
-const int m_pot_pin = A0;
-int m_pot_val {0};
-int m_pin_val;
+const int LedOnPin=4;
+const int LedOffPin=5;
+const int DhtSys1Pin=6;
+int BlynkSignal;
 
 BLYNK_WRITE(V0) // LED high/low state
 {
   Serial.print("Outputting V0");
   // setting digital pin 6 to type OUTPUT
-  m_pin_val = param.asInt(); // assigning incoming value from pin V0 to a variable
+  BlynkSignal = param.asInt(); // assigning incoming value from pin V0 to a variable
   Serial.print("Converted to \n");
-  Serial.print(m_pin_val);
+  Serial.print(BlynkSignal);
+  delay(10);
   // You can also use:
   // String i = param.asStr();
   // double d = param.asDouble();
 
-  if (m_pin_val) {
-    digitalWrite(m_led_pin_high,1);
-    digitalWrite(m_led_pin_low,0);
+  if (BlynkSignal) {
+    digitalWrite(LedOnPin,1);
+    digitalWrite(LedOffPin,0);
+    delay(10);
     
   } else {
-     digitalWrite(m_led_pin_high,0);
-    digitalWrite(m_led_pin_low,1);
+     digitalWrite(LedOnPin,0);
+    digitalWrite(LedOffPin,1);
+    delay(10);
   }
 }
 
-void potWrite() {
-  if (!m_pin_val) {
-    Blynk.virtualWrite(V1, 0);
-    return;
-  }
+// void potWrite() {
+//   if (!BlynkSignal) {
+//     Blynk.virtualWrite(V1, 0);
+//     return;
+//   }
 
-  m_pot_val = analogRead(m_pot_pin);  // set potVal
-  Blynk.virtualWrite(V1, analogRead(m_pot_val)); // write potVal
+//   m_pot_val = analogRead(m_pot_pin);  // set potVal
+//   Blynk.virtualWrite(V1, analogRead(m_pot_val)); // write potVal
+// }
+
+void write() {
+  float humidity = m_dhtSys1.getHumidity();
+  float temperature = m_dhtSys1.getTemperature();
+  Blynk.virtualWrite(V2, temperature);
+  Blynk.virtualWrite(V3, humidity);
+  delay(10);
 }
 
 void setup()
@@ -81,20 +93,22 @@ void setup()
   EspSerial.begin(ESP8266_BAUD);
   delay(10);  // allows the serial connection to stabilize
 
+  m_dhtSys1.setup(DhtSys1Pin);
   Blynk.begin(auth, wifi, ssid, pass);
   // You can also specify server:
   //Blynk.begin(auth, wifi, ssid, pass, "blynk.cloud", 80);
   //Blynk.begin(auth, wifi, ssid, pass, IPAddress(192,168,1,100), 8080);
 
-   pinMode(m_led_pin_high,OUTPUT);
-   pinMode(m_led_pin_low,OUTPUT);
-   m_timer.setInterval(1000L, potWrite); // milliseconds datatype long; pointer to void callback
+   pinMode(LedOnPin,OUTPUT);
+   pinMode(LedOffPin,OUTPUT);
+   m_timer.setInterval(1000L, write); // milliseconds datatype long; pointer to void callback
 }
 
 void loop()
 {
   Blynk.run();
   m_timer.run();
+  delay(100);
   // You can inject your own code or combine it with other sketches.
   // Check other examples on how to communicate with Blynk. Remember
   // to avoid delay() function!
